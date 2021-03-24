83e30361c0fcc0263f7563acdcca6f45bce68acc
elenrobe <elenarobertnunez@gmail.com>
2021-03-24 11:29:48 +0100

    cosas

---
+++ Assets/Menggen/Script/Consoller.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Consoller : MonoBehaviour
+{
+    //Declarar las variables
+    public float velocity;
+    float deltaX, deltaY;
+    Rigidbody2D rb;
+    Vector2 vector;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Cacheamos el componente Rigidbody
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Obtenemos el input de la direccion y lo multiplicamos por la velocidad
+        vector = new Vector2(Input.GetAxis("Horizontal") * velocity, Input.GetAxis("Vertical") * velocity);
+        //Movimiento diagonal
+        if(deltaX!=0 && deltaY != 0)
+        {
+            vector = new Vector2(Input.GetAxis("Horizontal") * velocity / 2, Input.GetAxis("Vertical") * velocity / 2);
+        }
+    }
+    //Movemos al jugador en el fixedUpdate ya que se trata de un movimiento físico
+    private void FixedUpdate()
+    {
+        rb.velocity = vector;
+    }
+}
