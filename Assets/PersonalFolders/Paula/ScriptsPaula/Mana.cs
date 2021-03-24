7850ebd37d17fd5b93e94b63db8644af32218fd3
Vicnuel5 <vestre01@ucm.es>
2021-03-24 11:38:29 +0100

    organizacion de carpetas

---
+++ Assets/PersonalFolders/Paula/ScriptsPaula/Mana.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mana : MonoBehaviour
+{
+    [SerializeField]
+    private int maxMana;
+    private float mana;
+
+    [SerializeField]
+    private float manaRegSpeed;
+
+    void Awake()
+    {
+        mana = maxMana;
+    }
+
+    
+    void Update()
+    {
+        if (mana < maxMana)
+        {
+            mana += manaRegSpeed * Time.deltaTime;
+        }
+        else mana = maxMana;
+        /*if (Input.GetKeyDown("space"))
+        {
+            RestaMana(30);
+        }
+        Debug.Log(mana);*/
+    }
+    public void RestaMana(int cantidad) //Se invoca al realizar una habilidad elemental
+    {
+        if (mana >= cantidad)
+        {
+            mana -= cantidad;
+        }
+
+    }
+}
