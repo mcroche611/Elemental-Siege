c259d71ae446065f709677c6da50d5b8c598d0f1
elenrobe <elenarobertnunez@gmail.com>
2021-03-31 17:13:16 +0200

    espada hecha

--- Assets/PersonalFolders/Elena/ScriptsElena/Enemy2.cs
+++ Assets/PersonalFolders/Elena/ScriptsElena/Enemy2.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy : MonoBehaviour
+public class Enemy2 : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
