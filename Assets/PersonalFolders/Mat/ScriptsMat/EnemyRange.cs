12bf69aee819eab043822bd27474f18ba4d92011
mcroche611 <mcroche@ucm.es>
2021-04-02 17:31:23 +0200

    Añadida la primera versión del script para que los enemigos persigan al jugador

---
+++ Assets/PersonalFolders/Mat/ScriptsMat/EnemyRange.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+
+public class EnemyRange : MonoBehaviour
+{
+    [SerializeField]
+    Transform player;
+
+    [SerializeField]
+    float detectRange;
+
+    [SerializeField]
+    float speed;
+
+    float distToPlayer;
+    Vector2 vel;
+    int horizontal, vertical;
+
+    Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        distToPlayer = Vector2.Distance(transform.position, player.position);
+
+        if (distToPlayer < detectRange)
+        {
+            ChasePlayer();
+        }
+    }
+
+    void ChasePlayer()
+    {
+        if (transform.position.x < player.position.x)
+        {
+            horizontal = 1;
+        }
+        else if (transform.position.x > player.position.x)
+        {
+            horizontal = -1;
+        }
+
+        if (transform.position.y < player.position.y)
+        {
+            vertical = 1;
+        }
+        else if (transform.position.y > player.position.y)
+        {
+            vertical = -1;
+        }
+
+        vel = new Vector2(horizontal, vertical).normalized;
+       
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(vel.x * speed, vel.y * speed);
+    }
+}
