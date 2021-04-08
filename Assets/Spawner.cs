6fc447ff7513c204605129ff3652d31c861a61eb
Vicnuel5 <vestre01@ucm.es>
2021-04-08 18:09:49 +0200

    update cosas compañeros

---
+++ Assets/Spawner.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject enemigo;
+    [SerializeField] float tiempoSpawn;
+
+    private void Start()
+    {
+        InvokeRepeating("Spawn", 0f, tiempoSpawn);
+    }
+
+    private void Spawn()
+    {
+        Instantiate<GameObject>(enemigo, transform.position, enemigo.transform.rotation);
+    }
+}
