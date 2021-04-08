0c2a3d6170907a09fb77fc0e07783f73266948b6
paula <paulitaenlinea@gmail.com>
2021-04-08 17:17:25 +0200

    update

--- Assets/MainGame/Scripts/Mana.cs
+++ Assets/MainGame/Scripts/Mana.cs
@@ -8,32 +8,22 @@ public class Mana : MonoBehaviour
     private int maxMana;
     private float mana;
 
-    [SerializeField]
-    private float manaRegSpeed;
+    
 
     void Awake()
     {
         mana = maxMana;
     }
-
-    
-    void Update()
+    private void Start()
     {
-        if (mana < maxMana)
-        {
-            mana += manaRegSpeed * Time.deltaTime;
-        }
-        else mana = maxMana;
-        /*if (Input.GetKeyDown("space"))
-        {
-            RestaMana(30);
-        }*/
-        //Debug.Log(mana);
+        //InvokeRepeating("SumaMana", )
     }
+
     public bool RestaMana(int cantidad) //Se invoca al realizar una habilidad elemental, comprueba si se puede realizar el ataque y resta mana si es asi
     {
         bool manaSuficiente= (mana>=cantidad);
-
+        bool max = (mana == maxMana);
+        //Si hay suficiente mana como para restarle
         if (manaSuficiente)
         {
             mana -= cantidad;
@@ -41,6 +31,16 @@ public class Mana : MonoBehaviour
         //Debug.Log(mana);
         return manaSuficiente;
         
+    }
+    public void SumaMana(int cantidadRegenera) //Recupera cierta cantidad de mana cada cantidad determinada de tiempo por manaRegSpeed
+    {
+        //Suma cuando el mana sea menor que la cantidad maxima
+        //Suma hasta que llegue a maxMana
+        
+        if(mana<maxMana && (mana + cantidadRegenera) < maxMana)
+        {
+            mana += cantidadRegenera;
+        }
     }
     
 }
