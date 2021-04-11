d631d706167809aea5d2ea71d73f8cd538897954
Mennggen <menhu@ucm.es>
2021-04-11 18:20:28 +0200

    Vapolizado prueba

---
+++ Assets/PersonalFolders/Menggen/Scripts/Vapolizado preuba.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vapolizadopreuba : MonoBehaviour
+{
+    public GameObject areaEfecto;
+
+    [SerializeField] float escaladoDeDaño;
+    float ataque, bonoAgua, bonoFuego, bonoElectricidad;
+
+    private void Start()
+    {
+        ataque = GameManager.GetInstance().Stat("");
+        bonoAgua = GameManager.GetInstance().Stat("Agua");
+        bonoFuego = GameManager.GetInstance().Stat("Fuego");
+        bonoElectricidad = GameManager.GetInstance().Stat("Electricidad");
+    }
+
+    private float Formula()
+    {
+        return (ataque + bonoAgua + bonoFuego) * escaladoDeDaño;
+    }
+}
