4d0e28a1cf0b45eb681316c42bbdf279dabf4d52
paula <paulitaenlinea@gmail.com>
2021-04-11 18:14:21 +0200

    prefab rango sobrecargado

---
+++ Assets/PersonalFolders/Paula/ScriptsPaula/AEOSobrecargado.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AEOSobrecargado : MonoBehaviour
+{
+    [SerializeField] 
+    float diametro;
+    [SerializeField]
+    float knock;
+
+    //El gameobject va creciendo hasta llegar a un diametro maximo y despues se destruye
+    void Start() 
+    {
+        transform.localScale = new Vector2(diametro, diametro);
+        Destroy(gameObject, Time.deltaTime);
+    }
+    //Aquellos enemigos con los que colisione este trigger seran impulsados y se les restara vida
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.GetComponent<EnemyHealth>() != null) //comprueba si aquello con lo que colisiona es un enemigo
+        {
+            //Knockback
+            Rigidbody2D rbEnemy = collision.GetComponent<Rigidbody2D>();
+            Vector2 direction = (rbEnemy.transform.position - transform.position).normalized;
+            rbEnemy.AddForce(direction * knock, ForceMode2D.Impulse);
+        } 
+    }
+}
