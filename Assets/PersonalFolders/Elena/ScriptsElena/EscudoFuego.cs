57e90520852be0ec510b82b380c9b7286e2baded
Vicnuel5 <vestre01@ucm.es>
2021-04-11 18:12:36 +0200

    corrección de fórmula electrocaragado

---
+++ Assets/PersonalFolders/Elena/ScriptsElena/EscudoFuego.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EscudoFuego : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+    }
+}
