4cfd447ca9b3afc894a21ddfe38dfb131b03120e
Mennggen <menhu@ucm.es>
2021-04-12 17:26:33 +0200

    Inicio del Script de Slime

--- Assets/PersonalFolders/Menggen/Scripts/SlimeOnDestroy.cs
+++ Assets/PersonalFolders/Menggen/Scripts/SlimeOnDestroy.cs
@@ -4,15 +4,15 @@ using UnityEngine;
 
 public class SlimeOnDestroy : MonoBehaviour
 {
+    Rigidbody2D rb;
+    GameObject SpriteSlime;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
+
+
