d56216a9cb46a9454d7e0b7d77b51c4b19cdbae6
Elena R <elenrobe@ucm.es>
2021-04-14 09:12:35 +0200

    implementado scripts de archer, prefab de flecha y empezado el escudo de fuego y fire guard

---
+++ Assets/PersonalFolders/Elena/ScriptsElena/FlechaMoveAttack.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlechaMoveAttack : MonoBehaviour
+{
+    [SerializeField]
+    float speed = 10;
+    [SerializeField]
+    float arrowAttack = 15;
+    Rigidbody2D rb;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.velocity = transform.up * speed;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.GetComponent<PlayerController>() != null)
+        {
+            collision.GetComponent<Health>().ReceiveDamage(arrowAttack);
+        }
+    }
+}
