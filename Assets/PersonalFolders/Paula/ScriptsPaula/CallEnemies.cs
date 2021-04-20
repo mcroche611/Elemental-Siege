f37b15c852daab29df85f86854046d3ed9172685
PaulaMorillasAlonso <pamorill@ucm.es>
2021-04-20 22:16:13 +0200

    creacion scripts para boss final que invoca enemigos

---
+++ Assets/PersonalFolders/Paula/ScriptsPaula/CallEnemies.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CallEnemies : MonoBehaviour
+{
+    [SerializeField] GameObject objetoSpawner;
+    Spawner scriptSpawner;
+    void Start()
+    {
+        //scriptSpawner = objetoSpawner.gameObject.GetComponent<Spawner>();
+    }
+
+    
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            objetoSpawner.SetActive(true);
+        }
+        else
+        {
+            objetoSpawner.SetActive(false);
+        }
+    }
+    private void OnDestroy() //destruir spawner cuando se destruya a iblis
+    {
+        Destroy(objetoSpawner);
+    }
+}
