d072a9987a478f05c6d7b6f3eb467ca13125e663
Vicnuel5 <vestre01@ucm.es>
2021-04-24 20:03:19 +0200

    Paso de escenas arreglados
    Hora la cinemachine sigue al jugador debido a la implementación de un level manager, el cual controla las puertas de la escena también

---
+++ Assets/MainGame/Scripts/Provisional.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Provisional : MonoBehaviour
+{
+    public GameObject gameManager;
+
+    void Start()
+    {
+        if (GameManager.GetInstance() == null)
+            Instantiate<GameObject>(gameManager, transform.position, transform.rotation);
+    }
+}
