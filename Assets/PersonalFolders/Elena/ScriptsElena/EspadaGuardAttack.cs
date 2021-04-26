e890a308f76479d371749500253be06df710de07
Elena R <elenrobe@ucm.es>
2021-04-26 08:43:39 +0200

    Cambio en el ataque del guard

---
+++ Assets/PersonalFolders/Elena/ScriptsElena/EspadaGuardAttack.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class EspadaGuardAttack : MonoBehaviour
+{
+    [SerializeField] 
+    float knock;
+
+    [SerializeField]
+    float attackDamage = 15f;
+    //Enemy enemy;
+
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.GetComponent<Mana>() != null)
+        {
+            //Knockback
+            Rigidbody2D rbEnemy = collision.GetComponent<Rigidbody2D>();
+            Vector2 direction = (rbEnemy.transform.position - transform.position).normalized;
+            rbEnemy.AddForce(direction * knock, ForceMode2D.Impulse);
+            //Aturdimiento
+            collision.GetComponent<EnemyMovement>().enabled = false;
+            //QuitarVida
+            collision.GetComponent<EnemyHealth>().QuitarVida(attackDamage);
+        }
+    }
+}
+
