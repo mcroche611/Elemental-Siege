203dbc5d08d7d14d1ab14eab4bec3579d02f73e8
Vicnuel5 <vestre01@ucm.es>
2021-05-01 22:01:04 +0200

    Arreglado bug por el cual tanto nasnas como los enemigos de espada eran indeplazables cuando atacaban

--- Assets/MainGame/Scripts/Enemigos/Específicos/EspadaGuard.cs
+++ Assets/MainGame/Scripts/Enemigos/Específicos/EspadaGuard.cs
@@ -6,17 +6,15 @@ public class EspadaGuard : MonoBehaviour
     [SerializeField]
     float attackCoolDown = 1.5f;
     [SerializeField]
-    float attackDuration = 1f; //cuidado con esto para que no salga negativo
+    float attackFreeze = 1f; //cuidado con esto para que no salga negativo
 
     GameObject guardSword;
-    Rigidbody2D enemyRigidbody;
 
     bool attackEnabled = true;
     [HideInInspector] public bool paralizado = false;
 
     private void Start()
     {
-        enemyRigidbody = GetComponentInParent<Rigidbody2D>();
         guardSword = transform.GetChild(0).gameObject;
     }
 
@@ -24,11 +22,11 @@ public class EspadaGuard : MonoBehaviour
     {
         if (attackEnabled && !paralizado)
         {
-            Freeze();  //se quita el movimiento del enemigo
+            GetComponentInParent<EnemyMovement>().Knockback(attackFreeze);
             guardSword.transform.rotation = Rotation(collision.transform.position);
             guardSword.SetActive(true);   //nada más entrar se activa la espada
 
-            Invoke("Unfreeze", attackDuration); //se le devuelve el movimiento pasado e ataque
+            Invoke("Unfreeze", attackFreeze); //se le devuelve el movimiento pasado e ataque
             Invoke("SwordDeactivate", Time.deltaTime); //y se desactiva acabada la duracion del ataque
 
             attackEnabled = false;
@@ -40,11 +38,10 @@ public class EspadaGuard : MonoBehaviour
     {
         if (attackEnabled && !paralizado)
         {
-            Freeze();  //se quita el movimiento del enemigo
+            GetComponentInParent<EnemyMovement>().Knockback(attackFreeze);
             guardSword.transform.rotation = Rotation(collision.transform.position);
             guardSword.SetActive(true);    //nada más entrar se activa la espada
 
-            Invoke("Unfreeze", attackDuration); //se le devuelve el movimiento pasado e ataque
             Invoke("SwordDeactivate", Time.deltaTime); //y se desactiva acabada la duracion del ataque
 
             attackEnabled = false;
@@ -68,18 +65,6 @@ public class EspadaGuard : MonoBehaviour
         Vector2 vector = new Vector2(posJugador.x - transform.position.x, posJugador.y - transform.position.y);
         float angulo = Mathf.Atan2(vector.y, vector.x) * 180 / Mathf.PI;
         return Quaternion.Euler(new Vector3(0f, 0f, angulo - 90));
-    }
-
-    private void Unfreeze()
-    {
-        enemyRigidbody.constraints = RigidbodyConstraints2D.None;
-        enemyRigidbody.constraints = RigidbodyConstraints2D.FreezeRotation;
-    }
-
-    private void Freeze()
-    {
-        CancelInvoke("Unfreeze");
-        enemyRigidbody.constraints = RigidbodyConstraints2D.FreezeAll;
     }
 
     private void EnableAttack()
