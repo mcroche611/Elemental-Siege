0fc85fb7c8d866c4624b2d54078ef738e36e08f6
PaulaMorillasAlonso <pamorill@ucm.es>
2021-05-03 00:10:10 +0200

    cambio en script ActivateShield

--- Assets/MainGame/Scripts/Enemigos/Específicos/ActivateShield.cs
+++ Assets/MainGame/Scripts/Enemigos/Específicos/ActivateShield.cs
@@ -4,13 +4,13 @@ using UnityEngine;
 
 public class ActivateShield : MonoBehaviour
 {
-    // Start is called before the first frame update
+    Escudo scriptEscudo;
     void Start()
     {
-        
+        scriptEscudo = GetComponent<Escudo>();
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
         
