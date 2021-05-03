8d340d54ac20fdf07743a4ec64c2901af680bc11
Mennggen <menhu@ucm.es>
2021-05-03 21:14:17 +0200

    Panel de pause completo

--- Assets/MainGame/Scripts/GestionEscenas/Menu.cs
+++ Assets/MainGame/Scripts/GestionEscenas/Menu.cs
@@ -5,8 +5,27 @@ using UnityEngine;
 
 public class Menu : MonoBehaviour
 {
-    public GameManager pauseMenu;
-    
+    public GameObject pauseMenu;
+    bool gamePaused;
+    private void Start()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (Time.timeScale == 1f)
+            {
+                Pause();
+            }
+            else
+            {
+                Resume();
+            }
+        }
+
+    }
     //Método público para cambiar de escena
     public void ChangeScene(string sceneName)
     {
@@ -19,5 +38,17 @@ public class Menu : MonoBehaviour
     public void Pause()
     {
         pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        gamePaused = true;
+    }
+    public void Resume()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        gamePaused = false;
+    }
+    public bool GamePaused()
+    {
+        return gamePaused;
     }
 }
