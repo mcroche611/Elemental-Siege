693e3f89d0693f27f7d8101bbcb894a0844fc5d5
Mennggen <menhu@ucm.es>
2021-05-03 19:05:14 +0200

    Cambiado los codigo de boton a otro script

---
+++ Assets/MainGame/Scripts/GestionEscenas/Menu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class Menu : MonoBehaviour
+{
+
+    //Método público para cambiar de escena
+    public void ChangeScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+    public void Quit()
+    {
+        UnityEditor.EditorApplication.isPlaying = false;
+    }
+
+}
