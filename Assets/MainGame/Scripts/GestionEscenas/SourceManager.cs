c502aadd5dd668343a6c70e51c789d306110e07b
Mennggen <menhu@ucm.es>
2021-05-13 23:36:08 +0200

    Efecto de sonido y banda sonora base hecho

---
+++ Assets/MainGame/Scripts/GestionEscenas/SourceManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SourceManager : MonoBehaviour
+{
+    public AudioSource audioSource;
+    public AudioClip playerWalking;
+    // Start is called before the first frame update
+    void Start()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
+    public void playerWalkungSound()
+    {
+        audioSource.clip = playerWalking;
+        audioSource.Play();
+    }
+}
