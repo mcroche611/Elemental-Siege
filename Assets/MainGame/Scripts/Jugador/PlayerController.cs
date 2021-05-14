6f8355eb5ccbac2fc7b06f02b7964bb1da84c889
Elena R <elenrobe@ucm.es>
2021-05-14 06:58:25 +0200

    Animación de Nasnas con flip

--- Assets/MainGame/Scripts/Jugador/PlayerController.cs
+++ Assets/MainGame/Scripts/Jugador/PlayerController.cs
@@ -13,9 +13,9 @@ public class PlayerController : MonoBehaviour
     Rigidbody2D rb;
     Vector2 vel;
 
-    bool runningRight = false; //bool para ver si el jugador se está moviendo en alguna dirección
+    bool seMueve = false; //bool para ver si el jugador se está moviendo en alguna dirección
                                //y animarlo
-    bool runningLeft = false;
+
     float aturdido = 0;
 
     void Start()
@@ -29,18 +29,27 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        animator.SetBool("seMueve", seMueve);
+
         RunningAnimation();
         Debug.Log("MOVIMIENTO HORIZONTAL" + Mathf.Abs(Input.GetAxisRaw("Horizontal")));
-        animator.SetBool("RunningRight", runningRight);
-        //animator.SetBool()
-        animator.SetBool("RunningLeft", runningLeft);
-        //animator.SetBool();
+
+
+
         if (aturdido <= 0)
         {
             //Input de movimiento de cuatro direccones y movimiento en diagonal
             vel = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")).normalized;
             //Velocidad de Movimiento
             rb.velocity = new Vector2(vel.x * velocity, vel.y * velocity);
+            if (rb.velocity.x < 0) //si en el eje de laas x está yendo a la izquierda, hacemos flip al sprite
+            {
+                gameObject.GetComponent<SpriteRenderer>().flipX = true;
+            }
+            else if (rb.velocity.x > 0) //no ponemos un else porque si no también se cambia al ir en vertical
+            {
+                gameObject.GetComponent<SpriteRenderer>().flipX = false;
+            }
         }
         else
             aturdido -= Time.deltaTime;
@@ -66,27 +75,11 @@ public class PlayerController : MonoBehaviour
     {
         if (Mathf.Abs(Input.GetAxisRaw("Horizontal")) > 0 || Mathf.Abs(Input.GetAxisRaw("Vertical")) > 0)
         {
-            runningRight = true;
-
-            if (Input.GetAxisRaw("Horizontal")<0)
-            {
-                runningRight = false;
-
-                runningLeft = true;
-            }
-            else if (Input.GetAxisRaw("Horizontal") > 0)
-            {
-                runningLeft = false;
-
-                runningRight = true;
-
-            }
+            seMueve = true;
         }
         else
         {
-            runningRight = false;
-            runningLeft = false;
-
+            seMueve = false;
         }
     }
 }
